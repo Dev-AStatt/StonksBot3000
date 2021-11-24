c16af6c5116a92927b413d4df477c4e37cb2196d
Aaron <AStatt@Protonmail.com>
2021-11-23 20:26:58 -0800

    adding graphing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #im sorry this has to be dt, i hate doing it but for some reason calling it datetime doesn't work?????
 from datetime import datetime
+import time
+import matplotlib.pyplot as plt
+from matplotlib import style
 from pingAPI import *
 from trades.tradeManager import *
 from training.trainingAPI import *
@@ -17,8 +20,9 @@ from training.trainingAPI import *
 #-----------------------------------------------------------------------------------------------------------#
 
 #   Required installs
-#   pip     <- to install pandas
-#   pandas  <- tool for importing and managing basically excel data
+#   pip         <- to install pandas
+#   pandas      <- tool for importing and managing basically excel data
+#   matplotlib  <- tool for displaying graphs and such things
 
 #   References:
 #   https://jamesgeorgedunn.com/2020/10/05/using-python-and-pandas-to-analyse-cryptocurrencies-with-coinapi/
@@ -29,21 +33,30 @@ from training.trainingAPI import *
 def main():
     #do not flag this as true unless you want to ping the API
     get_Historical_Data = False
-
+    run_training_simulation = True
+    display_graphs = True
     if(get_Historical_Data):
         api = CoinAPI__Interface()
         api.get_historical_data_and_save_csv("LTC/USD", "1HRS", "2020-01-01T00:00:00", "2021-10-31T23:59:00","100000")
         print("Sucsessful?")
 
-    #this will print the first 5 enteries of the structure. Use .tail() to get the last 5
-    #print(ltc_data.head())
-
-    #load training data
-    training = training_API(True)
-    training.update_ticker_data("LTC/USD")
-    training.update_ticker_data("LTC/USD")
     #testing feature of adding a trade and printing update
     trademan = tradeManager()
+    #load training data
+    training = training_API(True)
+    if(display_graphs):
+        #training.active_chart['Price Open'].plot()
+        ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+        ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+        ax1.plot(training.active_chart_1h.index, training.active_chart_1h['Price Open'])
+        ax1.plot(training.active_chart_1h.index, training.active_chart_1h['100ta'])
+        ax2.bar(training.active_chart_1h.index, training.active_chart_1h['Volume Traded'])
+        plt.show()
+
+    #Run training sumulation
+    while(run_training_simulation):
+        training.update_ticker_data("LTC/USD")
+        time.sleep(1)
     #adding a new trade
     trademan.new_trade("LTC",0.1,6000,datetime.now())
     trademan.print_trades()
--- training/trainingAPI.py
+++ training/trainingAPI.py
@@ -7,20 +7,25 @@ from util.table_import_refactor import *
 
 class training_API():
     """API that functions like it is pulling real data, however it will return historic data on file"""
+    active_chart_1d_ohlc = []
+    active_chart_1d_volume = []
     def __init__(self, debug_mode):
         file_exists = os.path.exists("_csv_files/History_FromAPI.csv")
         if not file_exists:
             print("File 'Historical_FromAPI.csv' does not exist.")
         #if exists
-        historical_data_raw = pd.read_csv("_csv_files/History_FromAPI.csv")
-        self._hidden_all_history_LTC = refactorData(historical_data_raw)
+        historical_data_raw = pd.read_csv('_csv_files/History_FromAPI.csv')
+        self._hidden_all_history_1h_LTC = refactorData(historical_data_raw)
         print("Load Training Data sucsess")
         #generate some history to start
-        self.active_chart = self._hidden_all_history_LTC[0:1000].copy()
-        self._row_last_delevered = len(self.active_chart)
-        #save to debug for inspection
+        self.active_chart_1h = self._hidden_all_history_1h_LTC[0:1000].copy()
+        self._row_last_delevered = len(self.active_chart_1h)
+         #add a moving average
+        self.active_chart_1h['100ta'] = self.active_chart_1h['Price Close'].rolling(window=100, min_periods=0).mean()
+
+       #save to debug for inspection
         if(debug_mode):
-            self.active_chart.to_csv("_csv_files/active_chart.csv", index = False)
+            self.active_chart_1h.to_csv("_csv_files/active_chart.csv", index = False)
 
     def update_ticker_data(self,symbol_id):
         """WIll update active_chart with new row of information"""
@@ -28,12 +33,24 @@ class training_API():
             print("Function in Training API not setup for this trade yet")
             return
         #get new row from hidden history
-        print("Row Last delevered" + str(self._row_last_delevered))
-        df_row = self._hidden_all_history_LTC[self._row_last_delevered:(self._row_last_delevered + 1)].copy()
+        df_row = self._hidden_all_history_1h_LTC[self._row_last_delevered:(self._row_last_delevered + 1)].copy()
         self._row_last_delevered = self._row_last_delevered + 1
-        print(df_row)
         #append row to end of dataframe
-        self.active_chart = self.active_chart.append(df_row)
-        print(self.active_chart.tail())
-        #self.active_chart.to_csv("_csv_files/active_chart.csv", index = False)
+        self.active_chart_1h = self.active_chart_1h.append(df_row)
+        self.update_df_analysis()
+        self.print_current_update()
 
+    #
+    #   This method of doing analysis on the new updates should be moved into its own class that handles heavy math
+    #
+    def update_df_analysis(self):
+        #re-calculate the moving average for the additional entery
+        self.active_chart_1h['100ta'] = self.active_chart_1h['Price Close'].rolling(window=100, min_periods=0).mean()
+        #Now refactor to get a 1 day dataframe from the 1h frame
+        #
+        #   This isnt working
+        #self.active_chart_1d_ohlc = self.active_chart_1h['Price Close'].resample('1D').ohlc()
+        print(self.active_chart_1d_ohlc)
+        
+    def print_current_update(self):
+        print(self.active_chart_1h[self._row_last_delevered -2:self._row_last_delevered -1])
