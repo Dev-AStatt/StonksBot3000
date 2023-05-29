c0b9657cc39cec99ac3d4388e2c6742833247717
Aaron-Win-Desktop <AStatt@protonmail.com>
2023-05-28 17:30:10 -0700

    Ping API is fixed and able to call the API

--- main.py
+++ main.py
@@ -69,15 +69,23 @@ def main():
     #do not flag this as true unless you want to ping the API
     get_Historical_Data = False
     run_training_simulation = True
-    display_graphs = False
+    display_ui = False
     if(get_Historical_Data):
         api = CoinAPI__Interface()
-        api.get_historical_data_and_save_csv("LTC/USD", "1HRS", "2020-01-01T00:00:00", "2021-10-31T23:59:00","100000")
-        print("Sucsessful?")
+        api.get_historical_data_and_save_csv(
+          "BITSTAMP_SPOT_BTC_USD",
+          "1HRS",
+          "2016-01-01T00:00:00"  
+        )
 
-    root = Tk()
-    GUI(root)
-    root.mainloop()
+        #api.get_historical_data_and_save_csv("LTC/USD", "1HRS", "2020-01-01T00:00:00", "2021-10-31T23:59:00","100")
+        
+
+    if(display_ui):
+        root = Tk()
+        GUI(root)
+        root.mainloop()
+    print("Done")
 
 
 if __name__ == '__main__':
--- pingAPI.py
+++ pingAPI.py
@@ -1,39 +1,68 @@
 import json
 import requests
 import pandas
+from config import *
 
 class CoinAPI__Interface():
 
     SANDBOX_base_url = 'https://rest-sandbox.coinapi.io/v1/ohlcv/'
     PRODUCTION_base_url = 'https://rest.coinapi.io/v1/ohlcv/'
-    headers = {'X-CoinAPI-Key' : 'Place_API_KEY_HERE'}
+    headers = {'X-CoinAPI-Key' : api_key}
 
-    def get_periods(self):
+
+
+    def print_periods(self):
         """Get full list of supported time periods availabe for requesting OHLCV data"""
         url = 'https://rest.coinapi.io/v1/ohlcv/periods'
-        #self._get_response(url,self.headers)
+        response = requests.get(url, headers=self.headers)
+        print(response.text)
+
+
+
+    def print_symbols(self):
+        """Get full list of supported symbols available"""
+        url = 'https://rest.coinapi.io/v1/symbols'
+        response = requests.get(url, headers=self.headers)
+        print(response)
+        print(response.text)
+
+
+    def _check_response(self, response):
+        if(response.status_code == 429):
+            print("Too many requests.")
+            return False
+        elif(response.status_code == 401):
+            print("Unauthorized -- Your API Key is wrong")
+            return False
+        elif(response.status_code == 400):
+            print("Bad Request -- There is something wrong with your request")
+            return False
+        elif(response.status_code == 403):
+            print("Forbidden -- Your API key doesn't have enough privilages to access this resource")
+            return False
+        elif(response.status_code == 404):
+            print("I think this is a bad URL?")
+            return False
+        elif(response.status_code == 550):
+            print("No data -- You requested specific single item that we don't have at this moment")
+            return False
+        
+        else:
+            return True
 
     def _get_response(self,url):
         response = requests.get(url, headers=self.headers)
+        print(response)
         #Check if API responds any Errors
-        if(response.status_code == 429):
-            print("Too many requests.")
-        if(response.status_code == 401):
-            print("Unauthorized -- Your API Key is wrong")
-        if(response.status_code == 400):
-            print("Bad Request -- There is something wrong with your request")
-        if(response.status_code == 403):
-            print("Forbidden -- Your API key doesn't have enough privilages to access this resource")
-        if(response.status_code == 550):
-            print("No data -- You requested specific single item that we don't have at this moment")
-
-        else:
+        if(self._check_response):
             # Get the information from the API
             coin_data  = json.loads(response.text)
             # Assign information to a DataFrame for later use
             data = pandas.DataFrame(coin_data)
             #Return data returned from api as pandas dataframe
             return data
+        else:
+            return False
 
     def get_latest_sandbox_data(self, symbol_id, period, limit):
         """Sandbox Interface with Coin API"""
@@ -54,20 +83,20 @@ class CoinAPI__Interface():
         url = url + '&include_empty_items={include_empty_items}'
         return self._get_response(url)
 
-    def _get_historical_data(self, symbol_id, period, time_start, time_end, limit):
+    def _get_historical_data(self, symbol_id, period, time_start, limit = "100"):
         """Private Method Get historical data """
         #GET /v1/ohlcv/{symbol_id}/history?period_id={period_id}&time_start={time_start}&time_end={time_end}&limit={limit}&include_empty_items={include_empty_items}
         url = self.PRODUCTION_base_url + symbol_id
         url = url + '/history?period_id=' + period
         url = url + '&time_start=' + time_start
-        url = url + '&time_end=' + time_end
+        #url = url + '&time_end=' + time_end
         url = url + '&limit=' + limit
         return self._get_response(url)
 
-    def get_historical_data_and_save_csv(self, symbol_id, period, time_start, time_end, limit):
+    def get_historical_data_and_save_csv(self, symbol_id, period, time_start, limit="100"):
         """method will call api for historical data and save to csv for training"""
         #get historical data from historical data method
-        historical_data = self._get_historical_data(symbol_id,period, time_start, time_end, limit)            # Create a CSV file with the values so not to waste the free daily API calls
+        historical_data = self._get_historical_data(symbol_id,period, time_start, limit)            # Create a CSV file with the values so not to waste the free daily API calls
         historical_data.to_csv("_csv_files/History_FromAPI.csv", index = False)
 
 
